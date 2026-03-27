73558b83f050853ea0dc53f65a63989cd72eddf3
varshasharma01 <varshasinbox1@gmail.com>
2026-03-27 15:41:02 +0530

    update column transformer code

--- My_Preprocess1/Column_Transformer.py
+++ My_Preprocess1/Column_Transformer.py
@@ -6,26 +6,48 @@ from .my_pipeline import MyPipeline
 import numpy as np
 import pandas as pd
 
+
 class MyColumnTransformer:
 
     def __init__(self, transformers):
         self.transformers = transformers
+        self.fitted_transformers = []
 
-    def fit_transform(self, df):
-        outputs = []
+    def fit(self, df):
+        self.fitted_transformers = []
 
         for name, encoder, col in self.transformers:
             
-            # it will extract column data
-            data = df[col]
-            # data = np.array(data).reshape(-1, 1)
-            transformed = encoder.fit_transform(data)
+            data = df[col].values.reshape(-1, 1)
+
+            # fit only
+            encoder.fit(data)
+
+            # store fitted encoder
+            self.fitted_transformers.append((name, encoder, col))
+
+        return self
+
+    def transform(self, df):
+        outputs = []
+
+        for name, encoder, col in self.fitted_transformers:
+            
+            data = df[col].values.reshape(-1, 1)
+
+            transformed = encoder.transform(data)
+
+            #  ensure 2D
+            if len(transformed.shape) == 1:
+                transformed = transformed.reshape(-1, 1)
 
             outputs.append(transformed)
 
-        # this will combine all outputs
         return np.concatenate(outputs, axis=1)
 
+    def fit_transform(self, df):
+        self.fit(df)
+        return self.transform(df)
 
 # df = pd.read_csv("Practice/SalaryData.csv")
 
