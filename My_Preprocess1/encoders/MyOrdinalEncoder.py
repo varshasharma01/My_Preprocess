d2a123842bc1209b1aac166098a2754486a0a726
varshasharma01 <varshasinbox1@gmail.com>
2026-03-27 11:20:50 +0530

    update MyOrdinalEncoder.py

--- My_Preprocess1/encoders/MyOrdinalEncoder.py
+++ My_Preprocess1/encoders/MyOrdinalEncoder.py
@@ -1,9 +1,9 @@
-import numpy as np 
-import pandas as pd
+# import numpy as np 
+# import pandas as pd
 
-class MyOrdinalEncoder:
+# class MyOrdinalEncoder:
     
-    # def ordinal_encoding(self, X):
+#     # def ordinal_encoding(self, X):
         
     #     order = {
     #         "Strong": 1,
@@ -12,37 +12,48 @@ class MyOrdinalEncoder:
     #     #   we can use for loop but this is faster to do
     #     return X.map(order)
     
+import numpy as np
+import pandas as pd
+
+class MyOrdinalEncoder:
+
     def __init__(self):
         self.mapping = None
-        
-    def fit(self,X, order):
+        self.most_frequent = None
+
+    def fit(self, X, order):
         self.mapping = {}
-        
         for i, category in enumerate(order):
             self.mapping[category] = i
-            
-        self.mapping = self.mapping
+
+        # Convert to pandas Series to easily drop NaN and find mode
+        X_series = pd.Series(X).dropna()
+        most_frequent_category = X_series.mode()[0]  # mode() returns sorted Series, take first
+        self.most_frequent = self.mapping.get(most_frequent_category, 0)
+
         return self
 
-    
     def transform(self, X):
         if self.mapping is None:
-            raise ValueError("first insert the order")
-        
+            raise ValueError("Call fit() before transform()")
+
+        X_array = np.array(X)  # convert to numpy array
+
         result = []
-        for value in X:
-            if value in self.mapping is None:
+        for value in X_array:
+            # check for NaN using pandas (works for both str and float NaN)
+            if pd.isna(value):
+                result.append(self.most_frequent)   # fill NaN with mode
+            elif value in self.mapping:
                 result.append(self.mapping[value])
-                
             else:
-                result.append(-1)
-     
+                result.append(self.most_frequent)   # fill unseen category with mode
+
         return np.array(result).reshape(-1, 1)
-    
+
     def fit_transform(self, X, order):
-        self.fit(X,order)
+        self.fit(X, order)
         return self.transform(X)  
-    
          
 # df = pd.read_csv("Practice/covid_data.csv")
 # X = df['cough']
--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from setuptools import setup, find_packages
 # from .My_Preprocess1.my_pipeline import MyPipeline
 
-
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
     name="varsha-preprocess",
-    version="0.1.1",   
+    version="0.1.2",   
     packages=find_packages(),
     install_requires=["numpy", "pandas"],
 
